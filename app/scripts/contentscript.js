9731b1002e8dd77ca4269fa5fd6584be48490bfc
honoka <terufumi.morishita.wp@hitachi.com>
2018-10-02 20:53:03 +0900

    [Update]

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -23,7 +23,6 @@ function sendMessage(keyboardEvent){
                 chrome.runtime.sendMessage({method: "copyUrlOfAllTab"}, response=>{});
             }
         }else{
-            console.log(keyboardEvent.key);
             if(keyboardEvent.key === "y"){
                 util.copyTextToClipboard("* [" + getHeadingText() + "](" + decodeURIComponent(location.href) + ")");
             }else if(keyboardEvent.key === "7"){  // This overrides ordinal ctrl+c. DO NOT.
